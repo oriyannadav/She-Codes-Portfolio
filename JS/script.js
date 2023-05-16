9b31a29f77e002d88159c234b30906045634b275
Oriyan Nadav <oriyanadav5@gmail.com>
2023-05-16 19:28:24 +1000

    She_Codes_Portfolio

---
+++ JS/script.js
@@ -0,0 +1,16 @@
+import Typed from 'typed.js';
+
+var typed = new Typed('.element', {
+    strings: ["First sentence.", "Second sentence."],
+    typeSpeed: 30,
+    backspeed: 30
+}); 
+
+// var typed = new Typed('.element', {
+//     strings: ["I'm Oriyan.", "I'm A Full Stack Web Developer."],
+//     typeSpeed: 70,
+//     startDelay: 900,
+//     backSpeed: 70,
+//     backDelay: 800,
+//     // loop: true
+// }); 
